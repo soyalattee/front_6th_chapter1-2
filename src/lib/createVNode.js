ec4d775867fd4ea2425952a1b93040a92bc785d9
soyalattee <uyyu1734@gmail.com>
2025-07-16 01:18:40 +0900

    feat: createVNode 함수 작업

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,19 @@
+// 평탄화 하기 위해 재귀적으로 평탄화 해줌, 그리고 falsy 값들 제거!
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: makeFlat(children).filter(
+      (child) => child !== null && child !== undefined && child !== false && child !== true,
+    ),
+  };
 }
+
+const makeFlat = (arr) => {
+  return arr.reduce((acc, curr) => {
+    if (Array.isArray(curr)) {
+      return acc.concat(makeFlat(curr));
+    }
+    return acc.concat(curr);
+  }, []);
+};
