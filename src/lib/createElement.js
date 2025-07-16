0049b8997383b3bb781079d7d064c63b7576603f
soyalattee <uyyu1734@gmail.com>
2025-07-16 15:50:09 +0900

    feat: createElement 테스트 통과
    처음, vNode 가 들어온 후 이 함수가 뭘 return해주는건지 몰라서 헤멤
    테스트케이스를 보고 el를 내보내야한다는걸 깨달음
    
    근데 throw new Error는 안해줘도 테스트케이스가 통과되는거같다
    
    createElement에서는 각 node를 불필요한(falsy값)이면 빈문자열화, 배열이면 DocumentFragment화 한 후 ,
    element를 생성해서 내보낸다.

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,45 @@
-import { addEvent } from "./eventManager";
+//import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || vNode === false || vNode === true) {
+    return { nodeType: Node.TEXT_NODE, textContent: "" };
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return { nodeType: Node.TEXT_NODE, textContent: vNode + "" };
+  }
 
-function updateAttributes($el, props) {}
+  if (Array.isArray(vNode)) {
+    return {
+      nodeType: Node.DOCUMENT_FRAGMENT_NODE,
+      childNodes: vNode.map((child) => createElement(child)),
+    };
+  }
+  if (typeof vNode === "function") {
+    console.log("컴포넌트를 직접 사용할 수 없습니다. 정규화 필요"); //안되는것같다.
+    throw new Error("컴포넌트를 직접 사용할 수 없습니다. 정규화 필요");
+  }
+
+  const el = document.createElement(vNode.type);
+  if (vNode.props) {
+    updateAttributes(el, vNode.props);
+  }
+
+  vNode.children.forEach((child) => {
+    if (child.type) {
+      el.appendChild(createElement(child));
+    } else {
+      el.innerHTML += child;
+    }
+  });
+  return el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
