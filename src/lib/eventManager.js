35e43e7ada1a250b78bc54d61548d3875ac346a2
soyalattee <uyyu1734@gmail.com>
2025-07-16 17:43:40 +0900

    feat: 이벤트 메니저 테스트 통과
    eventRegistry 배열을 만들어 이벤트를 이곳에 등록하고, setup시 root엘리먼트에 등록되도록 함
    
    두번째 테스트코드가 실행되며
    기존 container를 제거하고 새로운 컨테이너를 붙히는작업을하는데(beforeEach, afterEach) 이때 eventRegistry가 비워지지않아서 두번째 테스트에서도 이미 이벤트가 붙어있는 문제 발생
    => setup시 eventRegistry를 정리하는 작업 추가. filter를 사용해 document에 해당 element가 없으면 제거

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,31 @@
-export function setupEventListeners(root) {}
+// 이벤트 목록 저장소
+let eventRegistry = [];
 
-export function addEvent(element, eventType, handler) {}
+//root 에 이벤트 리스너 등록
+export function setupEventListeners(root) {
+  // DOM에서 제거된 element들의 이벤트 정리
+  eventRegistry = eventRegistry.filter((entry) => document.contains(entry.element));
+  eventRegistry.forEach(({ eventType }) => {
+    root.addEventListener(eventType, (e) => {
+      eventRegistry
+        .filter((entry) => entry.eventType === eventType)
+        .forEach(({ element, handler }) => {
+          if (element.contains(e.target)) {
+            e.stopPropagation();
+            handler(e);
+          }
+        });
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  eventRegistry.push({ element, eventType, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  eventRegistry = eventRegistry.filter(
+    (entry) => !(entry.element === element && entry.eventType === eventType && entry.handler === handler),
+  );
+  console.log("eventRegistry", eventRegistry);
+}
