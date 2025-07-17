b081616148545cfac8ddc397cda02a3de965c331
soyalattee <uyyu1734@gmail.com>
2025-07-18 01:44:19 +0900

    Revert "빌드갱신"
    This reverts commit 9e5429bc726f2174c3b179ffd90700ddb044de15.

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,17 +1,5 @@
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
-const basePath = import.meta.env.PROD ? "/front_6th_chapter1-1" : "";
 
 // MSW 워커 설정
 export const worker = setupWorker(...handlers);
-
-export const workerOptions = import.meta.env.PROD
-  ? {
-      serviceWorker: {
-        url: `${basePath}/mockServiceWorker.js`,
-      },
-      onUnhandledRequest: "bypass",
-    }
-  : {
-      onUnhandledRequest: "bypass",
-    };
