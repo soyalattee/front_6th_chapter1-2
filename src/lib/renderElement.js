eed2ab9a61bf03250cd4a4e2ca276b278e94669f
soyalattee <uyyu1734@gmail.com>
2025-07-18 01:08:01 +0900

    fix: container 에 prevNode로 저장해두던 값 제거하고 currentNodeMap을 저장하는 맵 으로 생성&렌더링 여부  결정

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,29 +3,24 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+const currentNodeMap = new WeakMap();
 export function renderElement(vNode, container) {
   const normalizedVNode = normalizeVNode(vNode);
 
-  // 최초 렌더링인지 확인 (이전 vNode가 없으면 최초)
-  if (!container._prevVNode) {
+  // 최초 렌더링인지 확인 (이전 currentNodeMap 없으면 최초)
+  const curNode = currentNodeMap.get(container);
+  if (!curNode) {
     // 최초 렌더링: createElement로 DOM 생성
     console.log("최초 렌더링");
     const $el = createElement(normalizedVNode);
     container.innerHTML = ""; // 기존 내용 제거
     container.appendChild($el);
-
-    // 이전 vNode 저장
-    container._prevVNode = vNode;
     // 렌더링 완료 후 이벤트 등록
     setupEventListeners(container);
   } else {
     // 업데이트: updateElement로 기존 DOM 업데이트
     console.log("업데이트 렌더링");
-    const prevNormalizedVNode = normalizeVNode(container._prevVNode);
-    // container.replaceChild(createElement(normalizedVNode), container.firstChild);
-    updateElement(container, normalizedVNode, prevNormalizedVNode);
-
-    // 새로운 vNode로 업데이트
-    container._prevVNode = vNode;
+    updateElement(container, normalizedVNode, curNode);
   }
+  currentNodeMap.set(container, normalizedVNode);
 }
